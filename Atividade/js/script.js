6ec9abdc59c1b87386055c6c673ceca103ff2bd5
P Araujo <mxpedro.araujo11@gmail.com>
2024-03-27 17:09:22 -0300

    atividadeJogosVorazes

---
+++ Atividade/js/script.js
@@ -0,0 +1,29 @@
+exercicio1=()=> {
+    document.getElementById("resposta").innerHTML = "<h3> Resposta da atividade</h3>"
+for (let i = 1; i <= 10; i++) {
+    document.getElementById("resposta").innerHTML += `#${i}`;
+    }
+};
+
+/* exercicio2 */
+exercicio2=()=> {
+    document.getElementById("resposta").innerHTML = "<h3> Resposta da atividade</h3>"
+    let num = document.getElementById ("num").value
+    for (let i = 0; i <= num ; i+= 2) {
+        console.log(i)  
+        document.getElementById("resposta").innerHTML += `#${i}`;
+
+    }
+};
+/* exercicio3 */
+exercicio3=()=> {
+for (let i = 0; i <  i++) {
+
+    
+}
+    if () {
+        
+    }
+
+}
+
